33b1f1cbf473ef5d972bdc62f9fa374f94d4b545
robert <ppabc@qq.com>
2016-12-13 13:12:03 +0800

    增加验证码,iptables初始化功能

--- boyurl.php
+++ boyurl.php
@@ -29,6 +29,17 @@ fwrite($handle, "$ip");
 fwrite($handle, "\n");
 fclose($handle);
 
+//验证码
+function randomkeys($length)
+{
+$pattern='1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ';
+for($i=0;$i<$length;$i++)
+{
+$key .= $pattern{mt_rand(0,35)};
+}
+return $key;
+}
+
 function Html()
 {
 global $statusid;
@@ -36,6 +47,8 @@ global $statusid1;
 global $statusid2;
 global $statusid5;
 global $statusid6;
+global $statusid7;
+global $statusid8;
 global $clientid;
 global $clientid1;
 global $clientid2;
@@ -60,7 +73,9 @@ echo '
 <input type="radio" name="status"  value="1" '.$statusid1.' />允许
 <input type="radio" name="status"  value="5" '.$statusid5.' />删允许
 <input type="radio" name="status"  value="2" '.$statusid2.' />禁止
-<input type="radio" name="status"  value="6" '.$statusid6.' />删禁止<br>
+<input type="radio" name="status"  value="6" '.$statusid6.' />删禁止
+<input type="radio" name="status"  value="7" '.$statusid7.' />初始化
+<input type="radio" name="status"  value="8" '.$statusid8.' />不设置<br>
 <input type="text" name="set_ip"   value="'.$ip.'"><br>
 需执行的shell脚本:<br />
 <textarea name="shell_txt" rows="5" cols="40">echo ok</textarea><br>
@@ -71,19 +86,29 @@ echo '
 </html>';
 }
 
-//登录  
-if($_POST['login'] == ""){  
+//登录
+
+if($_POST['login'] == ""){
+session_start();
+$_SESSION['randomkeys'] = randomkeys(6);
+$randomkeys = $_SESSION['randomkeys'];
+//$_SESSION['randomkeys'] = randomkeys(6);
+//$randomkeys = $_SESSION['randomkeys']
 echo '<html>  
 <head>用户登录</head>  
 <form name="LoginForm" method="post" action="boyurl.php" onSubmit="return InputCheck(this)">  
 <p>  
 <label for="username" class="label">用户名:</label>  
-<input id="username" name="username" type="text" class="input" />  
+<input id="username" name="username" type="text" class="input" />
 <p/>  
-<p>  
+<p>
 <label for="password" class="label">密&nbsp;码:</label>  
 <input id="password" name="password" type="password" class="input" />  
-<p/>  
+<p/>
+<p>
+<label for="randomkeys" class="label">验证码:</label>  
+<input id="randomkeys" name="randomkeys" type="text" class="input" />'.$randomkeys.'
+<p/>
 <p>  
 <input type="hidden" name="login" value="1" />
 <input type="submit" name="submit" value="  确 定  " class="left" />  
@@ -97,17 +122,17 @@ if($_POST['login'] == "1"){
 if(!isset($_POST['submit'])){  
     exit('非法访问!');  
 }
-
-$username = htmlspecialchars($_POST['username']);  
-$password = MD5($_POST['password']); 
-
-//默认密码boyurl.com,md5加密值cf861e7add70d498d95d6e6763a87258
-if($username == "admin" and $password == "cf861e7add70d498d95d6e6763a87258"){  
-//登录成功  
 session_start();
 $_SESSION['username'] = $username;  
-$_SESSION['userid'] = $result['userid'];  
+$_SESSION['userid'] = $result['userid'];
 $clientid = $_SESSION['clientid'];
+$username = htmlspecialchars($_POST['username']);  
+$password = MD5($_POST['password']); 
+$keys = $_POST['randomkeys']; 
+
+//默认密码boyurl.com,md5加密值cf861e7add70d498d95d6e6763a87258  
+if($username == "admin" and $password == "cf861e7add70d498d95d6e6763a87258" and $_SESSION['randomkeys'] == $keys){  
+//登录成功  
 echo $username,' 欢迎你！点击此处 <a href="boyurl.php?action=logout">注销</a> 登录！<br />';
 echo '<br><br>';
 Html();
@@ -140,26 +165,70 @@ if($statusid==1) $statusid1 = "checked=checked";
 if($statusid==2) $statusid2 = "checked=checked"; 
 if($statusid==5) $statusid5 = "checked=checked"; 
 if($statusid==6) $statusid6 = "checked=checked";
+if($statusid==7) $statusid7 = "checked=checked"; 
+if($statusid==8) $statusid8 = "checked=checked";
 if($clientid==12)  $clientid1 = "checked=checked";
-if($clientid==6)  $clientid2 = "checked=checked";
+if($clientid==6)   $clientid2 = "checked=checked";
 if($clientid==30)  $clientid3 = "checked=checked";
 
-if ($statusid == "") {
-  $str_ipt = "echo ok";
-} else {
-	if ($statusid == "1") {
-  $str_ipt = "/sbin/iptables -A INPUT -s ".$set_ip." -j ACCEPT";
-	}
-	if ($statusid == "2") {
-  $str_ipt = "/sbin/iptables -A INPUT -s ".$set_ip." -j DROP";
-	}
-	if ($statusid == "5") {
-  $str_ipt = "/sbin/iptables -D INPUT -s ".$set_ip." -j ACCEPT";
-	}
-	if ($statusid == "6") {
-  $str_ipt = "/sbin/iptables -D INPUT -s ".$set_ip." -j DROP";
-	}
+switch ($statusid)
+{
+case 1:
+$str_ipt = "
+/sbin/iptables -D INPUT -j REJECT
+/sbin/iptables -A INPUT -s ".$set_ip." -j ACCEPT
+/sbin/iptables -A INPUT -j REJECT
+  ";
+  break;
+case 2:
+$str_ipt = "
+/sbin/iptables -D INPUT -j REJECT
+/sbin/iptables -A INPUT -s ".$set_ip." -j DROP
+/sbin/iptables -A INPUT -j REJECT
+  ";
+  break;
+case 5:
+$str_ipt = "
+/sbin/iptables -D INPUT -j REJECT
+/sbin/iptables -D INPUT -s ".$set_ip." -j ACCEPT
+/sbin/iptables -A INPUT -j REJECT
+  ";
+  break;
+case 6:
+$str_ipt = "
+/sbin/iptables -D INPUT -j REJECT
+/sbin/iptables -D INPUT -s ".$set_ip." -j DROP
+/sbin/iptables -A INPUT -j REJECT
+  ";
+  break;
+case 7:
+$str_ipt = "
+/sbin/iptables -F
+/sbin/iptables -t raw -F
+/sbin/iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
+/sbin/iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+/sbin/iptables -A INPUT -s ".$set_ip." -j ACCEPT
+/sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED,UNTRACKED -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 80  -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 443   -j ACCEPT
+/sbin/iptables -t raw -A PREROUTING -p tcp --dport 80  -j NOTRACK
+/sbin/iptables -t raw -A PREROUTING -p tcp --dport 443  -j NOTRACK
+/sbin/iptables -t raw -A OUTPUT -p tcp --sport 80  -j NOTRACK
+/sbin/iptables -t raw -A OUTPUT -p tcp --sport 443  -j NOTRACK
+/sbin/iptables -A OUTPUT -j ACCEPT
+/sbin/iptables -A INPUT -j REJECT
+/sbin/iptables -A FORWARD -j REJECT
+/sbin/service iptables save
+echo ok
+  ";
+  break;
+case 8:
+$str_ipt = "echo ok";
+  break;
+default:
+$str_ipt = "echo ok";
 }
+
 $str_txt = "
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
@@ -187,8 +256,8 @@ Lujing=`cat /tmp/boyurl_pid.txt | grep Lujing | tail -n 1 |awk -F ' ' '{print $2
 clientid1=`cat /tmp/boyurl_pid.txt | grep clientid | tail -n 1 |awk -F ' ' '{print $2}'`
 clientid2=`cat \$Lujing/boyurl_cron.txt | grep 'it is clientid' | tail -n 1 |awk -F ' ' '{print $5}'`
 if [ \$clientid1 -gt 10 ] && [ \$clientid2 -gt 10 ]; then 
-    $str_ipt
-    $str_shell
+$str_ipt
+$str_shell
 	echo 'it is clientid $clientid ok'
 	sed -i 's/clientid 12 ok/clientid 6 ok/g'  \$Lujing/boyurl_cron.txt
 	sed -i 's/clientid 12 ok/clientid 6 ok/g'  /tmp/boyurl_cron.txt
